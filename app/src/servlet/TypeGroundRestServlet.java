6b5db04b2dfc8a9e21705e588af645f75adfc41e
FacuDeveloper <Facu.s.nf@gmail.com>
2019-09-04 22:23:39 -0300

    Back TypeGround finished

---
+++ app/src/servlet/TypeGroundRestServlet.java
@@ -0,0 +1,72 @@
+package servlet;
+
+import javax.ejb.EJB;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+// import javax.ws.rs.QueryParam;
+// import javax.ws.rs.FormParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.core.MediaType;
+
+import model.TypeGround;
+
+import stateless.TypeGroundServiceBean;
+
+import java.util.Collection;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+
+@Path("/typeGround")
+public class TypeGroundRestServlet {
+
+  // inject a reference to the TypeGroundServiceBean slsb
+  @EJB TypeGroundServiceBean service;
+
+  //mapea lista de pojo a JSON
+  ObjectMapper mapper = new ObjectMapper();
+
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  public String findAll() throws IOException {
+    Collection<TypeGround> types = service.findAll();
+    return mapper.writeValueAsString(types);
+  }
+
+  @GET
+  @Path("/{id}")
+  @Produces(MediaType.APPLICATION_JSON)
+  public String find(@PathParam("id") int id) throws IOException {
+    TypeGround typeGround = service.find(id);
+    return mapper.writeValueAsString(typeGround);
+  }
+
+  @POST
+  @Consumes(MediaType.APPLICATION_JSON)
+  public String create(String json) throws IOException {
+    TypeGround newTypeGround = mapper.readValue(json, TypeGround.class);
+    newTypeGround = service.create(newTypeGround);
+    return mapper.writeValueAsString(newTypeGround);
+  }
+
+  @DELETE
+  @Path("/{id}")
+  @Produces(MediaType.APPLICATION_JSON)
+  public String remove(@PathParam("id") int id) throws IOException {
+    TypeGround typeGround = service.remove(id);
+    return mapper.writeValueAsString(typeGround);
+  }
+
+  @PUT
+  @Path("/{id}")
+  @Produces(MediaType.APPLICATION_JSON)
+  public String modify(@PathParam("id") int id, String json) throws IOException {
+    TypeGround modifiedTypeGround = mapper.readValue(json, TypeGround.class);
+    return mapper.writeValueAsString(service.modify(id, modifiedTypeGround));
+  }
+
+}
--- app/src/stateless/TypeGroundServiceBean.java
+++ app/src/stateless/TypeGroundServiceBean.java
@@ -3,6 +3,9 @@ package stateless;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import java.util.Collection;
 
 import model.TypeGround;
 
@@ -17,6 +20,61 @@ public  class TypeGroundServiceBean {
 
   public void setEntityManager(EntityManager localEntityManager){
     entityManager = localEntityManager;
+  }
+
+  public EntityManager getEntityManager() {
+    return entityManager;
+  }
+
+  public TypeGround create(TypeGround typeGround) {
+    getEntityManager().persist(typeGround);
+    return typeGround;
+  }
+
+  /**
+   * Elimina un suelo mediante su id
+   *
+   * @param  id
+   * @return No nulo en caso de haber eliminado el suelo, en caso contrario nulo
+   */
+  public TypeGround remove(int id) {
+    TypeGround typeGround = find(id);
+
+    if (typeGround != null) {
+      getEntityManager().remove(typeGround);
+      return typeGround;
+    }
+
+    return null;
+  }
+
+  /**
+   * Actualiza o modifica la entidad asociada al id dado
+   *
+   * @param  id
+   * @param  modifiedTypeGround
+   * @return un valor no nulo en caso de modificar la entidad solicitada
+   * mediante el id, en caso contrario retorna un valor nulo
+   */
+  public TypeGround modify(int id, TypeGround modifiedTypeGround) {
+    TypeGround choosenTypeGround = find(id);
+
+    if (choosenTypeGround != null) {
+      choosenTypeGround.setTexture(modifiedTypeGround.getTexture());
+      choosenTypeGround.setDescription(modifiedTypeGround.getDescription());
+      return choosenTypeGround;
+    }
+
+    return null;
+  }
+
+  public TypeGround find(int id) {
+    return getEntityManager().find(TypeGround.class, id);
+  }
+
+  public Collection<TypeGround> findAll() {
+    Query query = getEntityManager().createQuery("SELECT t FROM TypeGround t ORDER BY t.id");
+    return (Collection<TypeGround>) query.getResultList();
   }
 
 }
